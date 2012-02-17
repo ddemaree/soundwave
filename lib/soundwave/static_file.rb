506e39a9cf608480971a2ec44043767f1ef7e202
David Demaree <ddemaree@gmail.com>
2012-02-17 17:36:56 -0600

    Removed StaticFile#changed? in favor of superclass method

--- lib/soundwave/static_file.rb
+++ lib/soundwave/static_file.rb
@@ -2,15 +2,9 @@ module Soundwave
   class StaticFile < Soundwave::Document
     def write
       if changed?
-        FileUtils.mkdir_p(output_path.dirname)
+        FileUtils.mkdir_p(output_path.dirname.to_s)
         FileUtils.cp(@pathname, output_path)
       end
-    end
-
-    def changed?
-      file_d = Digest::MD5.file(output_path)
-      content_d = Digest::MD5.file(@pathname)
-      file_d != content_d
     end
   end
 end
